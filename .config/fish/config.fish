e9b3cf02624d0c2fee511f03f3d49bc5d08c5a5f
Xarianne <xariann.widely103@passmail.com>
2026-01-21 20:03:20 +0000

    Reorg fish configs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,5 +2,11 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+set -gx MERGE meld
+
+if test -d $HOME/.cargo/bin
+    fish_add_path $HOME/.cargo/bin
+end
+
 alias metapac-unsorted="metapac unmanaged > $HOME/.config/metapac/groups/unsorted.toml"
 alias code='flatpak run com.visualstudio.code'
