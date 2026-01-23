55c2a1733b8656d636a7143648390f69e27586c4
Xarianne <games.clinking002@passinbox.com>
2026-01-23 20:59:55 +0000

    Added missing packages

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,3 +10,5 @@ end
 
 alias metapac-unsorted="metapac unmanaged > $HOME/.config/metapac/groups/unsorted.toml"
 # alias code='flatpak run com.visualstudio.code'
+
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv fish)"
